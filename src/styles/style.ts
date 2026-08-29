c6c687babd4a1af3d57135dd7cb4d1372bd64942
moisesBarsoti <moises.barsot2@gmail.com>
2026-08-29 20:38:31 -0300

    style(styles): criado variáveis de cores

---
+++ src/styles/style.ts
@@ -0,0 +1,15 @@
+export const BACKGROUND = {
+    backgorundMain: '#F1EFFD'
+}
+
+export const COLORS = {
+    background: '#F1EFFD',
+    white: '#FEFEFE',
+    black: '#000000',
+    primary: '#7E61D5',
+    gray: '#ADADAD',
+    primarySoft: '#7E61D51F',
+    primaryBorder: '#7E61D555',
+    grayBorder: '#ADADAD66',
+    grayMuted: '#ADADADB0',
+};
